7436c87b630cedb9fc490cd1e8619d53c1c7fbe9
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-11-04 13:35:39 +1300

    cpu and memory skeleton within vue component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -11,10 +11,70 @@
         q-list-header
           p Essential Links
 
-    div(class="layout-padding logo-container non-selectable no-pointer-events")
+    div
+      q-btn(@click="cpu.jump(3)") jump(3)
 </template>
 
 <script>
+
+console.log('Hello.vue started')
+
+class Memory {
+  constructor () {
+    this.data = Array(256)
+    this.lastAccess = -1
+    this.reset()
+  }
+  reset () {
+    this.lastAccess = -1
+    for (var i = 0, l = this.data.length; i < l; i++) {
+      this.data[i] = 0
+    }
+  }
+  load (address) {
+    if (address < 0 || address > this.data.length) {
+      throw new Error('Memory access violation at ' + address)
+    }
+    this.lastAccess = address
+    return this.data[address]
+  }
+  store (address, value) {
+    if (address < 0 || address > this.data.length) {
+      throw new Error('Memory access violation at ' + address)
+    }
+    this.lastAccess = address
+    this.data[address] = value
+  }
+}
+
+class CPU {
+  constructor (memory) {
+    this.memory = new Memory()
+    this.reset()
+  }
+  reset () {
+    this.maxSP = 231
+    this.minSP = 0
+
+    // general purpose registers GPR0, GPR1, GPR2, GPR3
+    this.gpr = [0, 0, 0, 0]
+    this.sp = this.maxSP // stack pointer
+    this.ip = 0 // instruction pointer
+    // zero, carry, fault flags
+    this.zero = false
+    this.carry = false
+    this.fault = false
+  }
+  jump (newIP) {
+    if (newIP < 0 || newIP >= this.memory.data.length) {
+      throw new Error('IP outside memory')
+    }
+    else {
+      this.ip = newIP
+    }
+  }
+}
+
 import {
   QLayout,
   QToolbar,
@@ -44,6 +104,7 @@ export default {
   },
   data () {
     return {
+      cpu: new CPU()
     }
   },
   computed: {
